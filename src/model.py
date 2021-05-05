c3f5678f95f763d5cbb20caeed0d3d0207b8e7ad
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-05-05 21:12:40 +0530

    feature - decoder

--- src/model.py
+++ src/model.py
@@ -14,3 +14,14 @@ class Encoder(nn.Module):
     def forward(self, x):
         h = F.relu(self.fc1(x))
         return self.fc_mu(h), self.fc_logvar(h)
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim=20, hidden_dim=400, output_dim=784):
+        super().__init__()
+        self.fc1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, z):
+        h = F.relu(self.fc1(z))
+        return torch.sigmoid(self.fc2(h))
