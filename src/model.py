854da4c6412c73b066a0575dbf0792b640cb4e12
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-05-05 20:09:43 +0530

    wip 2 - encoder

--- src/model.py
+++ src/model.py
@@ -1,2 +1,16 @@
-"""VAE model: encoder, reparameterize, decoder."""
-# TODO: implement
+"""Vanilla VAE: MLP encoder, reparameterize trick, MLP decoder."""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=20):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc_mu = nn.Linear(hidden_dim, latent_dim)
+        self.fc_logvar = nn.Linear(hidden_dim, latent_dim)
+
+    def forward(self, x):
+        h = F.relu(self.fc1(x))
+        return self.fc_mu(h), self.fc_logvar(h)
