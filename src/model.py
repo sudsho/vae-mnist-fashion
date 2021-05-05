588a61c156795d8f198c71d543f1e62635806a82
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-05-05 23:12:04 +0530

    wip - reparam + vae wrapper

--- src/model.py
+++ src/model.py
@@ -25,3 +25,23 @@ class Decoder(nn.Module):
     def forward(self, z):
         h = F.relu(self.fc1(z))
         return torch.sigmoid(self.fc2(h))
+
+
+def reparameterize(mu, logvar):
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std)
+    return mu + eps * std
+
+
+class VAE(nn.Module):
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=20):
+        super().__init__()
+        self.encoder = Encoder(input_dim, hidden_dim, latent_dim)
+        self.decoder = Decoder(latent_dim, hidden_dim, input_dim)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        mu, logvar = self.encoder(x)
+        z = reparameterize(mu, logvar)
+        x_hat = self.decoder(z)
+        return x_hat, mu, logvar
