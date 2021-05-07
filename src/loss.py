433c3510804fff4b197e563739f14295cdf0fe52
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2021-05-07 18:42:30 +0530

    feature - elbo with beta

--- src/loss.py
+++ src/loss.py
@@ -1,2 +1,13 @@
 """ELBO loss with beta-weighted KL term."""
-# TODO
+import torch
+import torch.nn.functional as F
+
+
+def vae_loss(x_hat, x, mu, logvar, beta=1.0):
+    """Negative ELBO. Returns total, recon, kl."""
+    x = x.view(x.size(0), -1)
+    recon = F.binary_cross_entropy(x_hat, x, reduction="sum")
+    # KL divergence between N(mu, sigma^2) and N(0, 1)
+    kl = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    total = recon + beta * kl
+    return total, recon, kl
